6b7f7f6dcaaa1edf91d313114c4ea8375eaafff6
Kohei-kun-no <k.minoda1205@iCloud.com>
2022-02-26 12:18:14 +0900

    provider_mini_sample

---
+++ lib/count_mode/main_sub.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:mini_provider/count_mode/home_screen_sub.dart';
+import 'package:mini_provider/home_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
